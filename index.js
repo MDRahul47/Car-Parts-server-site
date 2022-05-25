18100aa5ee11d96de26c7e42b98a39c3ce915b3b
MDRahul47e <rahulahames@gmail.comm>
2022-05-25 21:16:28 +0600

    admin done

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+
+// start the main backend code
+
 async function run() {
 
     try {
